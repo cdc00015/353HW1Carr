939f4c55cd452ec25174184c74d81f0901a81d3d
carsonmealey <142524810+carsonmealey@users.noreply.github.com>
2024-02-27 20:12:17 -0500

    Update CarsonMealeySPs.sql

--- SQL/CarsonMealeySPs.sql
+++ SQL/CarsonMealeySPs.sql
@@ -20,7 +20,7 @@ create or alter proc spTeamGetAdvancedStats
 AS 
 BEGIN
 select T.TeamID, T.TeamName, T.City, T.Location, T.Division,
-ROUND(CAST(SUM (G.HomePoints + G.AwayPoints) AS FLOAT) / NULLIF(CAST(COUNT(G.GameID) AS INT) ,0) ,2) AS "Points Per Game", --Used ChatGPT 
+ROUND(CAST(SUM (G.HomePoints + G.AwayPoints) AS FLOAT) / NULLIF(CAST(COUNT(G.GameID) AS INT) ,0) ,2) AS "Points Per Game", --Used ChatGPT.  Prompt used was: "With the current database layout and data, how would I make a standard procedure to calculate the average points by a team per game?"
 ROUND(CAST(SUM (G.HomeYards + G.AwayYards) AS FLOAT) / NULLIF(CAST(COUNT(G.GameID) AS INT) ,0) ,2) AS "Yards Per Game" --Copied previous line of code and modified for yards
 from Team as T
 inner join GameTeam as GT on T.TeamID = GT.TeamID
