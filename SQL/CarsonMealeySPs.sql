8b45d54ade5ecd9ed4147b134e9f7430519507af
carsonmealey <142524810+carsonmealey@users.noreply.github.com>
2024-02-27 20:05:57 -0500

    Update CarsonMealeySPs.sql

--- SQL/CarsonMealeySPs.sql
+++ SQL/CarsonMealeySPs.sql
@@ -20,8 +20,8 @@ create or alter proc spTeamGetAdvancedStats
 AS 
 BEGIN
 select T.TeamID, T.TeamName, T.City, T.Location, T.Division,
-ROUND(CAST(SUM (G.HomePoints + G.AwayPoints) AS FLOAT) / NULLIF(CAST(COUNT(G.GameID) AS INT) ,0) ,2) AS "Points Per Game",
-ROUND(CAST(SUM (G.HomeYards + G.AwayYards) AS FLOAT) / NULLIF(CAST(COUNT(G.GameID) AS INT) ,0) ,2) AS "Yards Per Game"
+ROUND(CAST(SUM (G.HomePoints + G.AwayPoints) AS FLOAT) / NULLIF(CAST(COUNT(G.GameID) AS INT) ,0) ,2) AS "Points Per Game", --Used ChatGPT 
+ROUND(CAST(SUM (G.HomeYards + G.AwayYards) AS FLOAT) / NULLIF(CAST(COUNT(G.GameID) AS INT) ,0) ,2) AS "Yards Per Game" --Copied previous line of code and modified for yards
 from Team as T
 inner join GameTeam as GT on T.TeamID = GT.TeamID
 inner join Game as G on GT.GameID = G.GameID
