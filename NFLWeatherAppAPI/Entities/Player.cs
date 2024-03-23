5d1f1f741aaa66ab3d5ed44b644f4c4fbaad5c6e
jw00006 <jw00006@mix.wvu.edu>
2024-03-23 19:28:35 -0400

    Added Player entity

---
+++ NFLWeatherAppAPI/Entities/Player.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NFLWeatherAppAPI.Entities
+{
+    public class Player
+    {
+        [Key]
+        public int PlayerID { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        public int? JerseyNum { get; set; } // Nullable int
+        [Required]
+        public string Height { get; set; }
+        [Required]
+        public int Weight { get; set; }
+        [Required]
+        public int TeamID { get; set; }
+        
+        public string College { get; set; }
+    }
+}
