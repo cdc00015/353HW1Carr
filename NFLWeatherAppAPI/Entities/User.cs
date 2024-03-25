22a7e081163d02deb9883bfb336c6eafeb5acc50
cdc00015 <cdc00015@mix.wvu.edu>
2024-03-25 17:13:38 -0400

    Commit

---
+++ NFLWeatherAppAPI/Entities/User.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace NFLWeatherAppAPI.Entities
+{
+    public class User
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int UserID { get; set; }
+
+        [Required]
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        [Required]
+        public DateTime DoB { get; set; }
+    }
+}
