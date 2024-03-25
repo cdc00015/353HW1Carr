bf1fd3dbd8e4d6ecd5238007b1534f4234c440a9
cdc00015 <cdc00015@mix.wvu.edu>
2024-03-25 16:21:16 -0400

    Commiting

--- 353HW1Carr/wwwroot/js/site.js
+++ 353HW1Carr/wwwroot/js/site.js
@@ -2,3 +2,13 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+<script>
+    function toggleTableVisibility() {
+  var table = document.getElementById('jlStats');
+    if (table.style.display === 'none') {
+        table.style.display = 'block';
+  } else {
+        table.style.display = 'none';
+  }
+}
+</script>
---
+++ NFLWeatherAppAPI3/Entities/Game.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NFLWeatherAppAPI3.Entities
+{
+    public class Game
+    {
+        public int GameID { get; set; }
+        public int? HomePoints { get; set; }
+        public int? AwayPoints { get; set; }
+        public int? HomeCompletions { get; set; }
+        public int? AwayCompletions { get; set; }
+        public int? HomeAttempts { get; set; }
+        public int? AwayAttempts { get; set; }
+        public int? HomeYards { get; set; }
+        public int? AwayYards { get; set; }
+        public int? HomeTD { get; set; }
+        public int? AwayTD { get; set; }
+        public int? HomeINT { get; set; }
+        public int? AwayINT { get; set; }
+    }
+}
