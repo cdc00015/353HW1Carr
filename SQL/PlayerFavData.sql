45b362bbcad6d37ee5480839e2fae7eb2bd73bab
Jacob Williamson <145377681+jw00006@users.noreply.github.com>
2024-03-26 23:24:25 -0400

    Update PlayerFavData.sql

--- SQL/PlayerFavData.sql
+++ SQL/PlayerFavData.sql
@@ -1,68 +1,18 @@
 INSERT INTO [dbo].[PlayerFav] ([UserID], [PlayerID]) VALUES 
-(1, 1),
-(1, 3)
-GO
-
-INSERT INTO [dbo].[PlayerFav] ([UserID], [PlayerID]) VALUES 
-(2, 2),
-(2, 4),
-(2, 5)
-GO
-
-INSERT INTO [dbo].[PlayerFav] ([UserID], [PlayerID]) VALUES 
-(3, 1),
-(3, 3),
-(3, 6),
-(3, 7)
-GO
-
-INSERT INTO [dbo].[PlayerFav] ([UserID], [PlayerID]) VALUES 
-(4, 5),
-(4, 8)
-GO
-
-INSERT INTO [dbo].[PlayerFav] ([UserID], [PlayerID]) VALUES 
-(5, 2),
-(5, 4),
-(5, 6)
-GO
-
+(1, 1), (1, 3),
+(2, 2), (2, 4), (2, 5),
+(3, 1), (3, 3), (3, 6), (3, 7),
+(4, 5), (4, 8),
+(5, 2), (5, 4), (5, 6),
 -- Continue this pattern for users 6 through 10
 -- Example for user 6 with 4 favorite players
-INSERT INTO [dbo].[PlayerFav] ([UserID], [PlayerID]) VALUES 
-(6, 3),
-(6, 5),
-(6, 7),
-(6, 9)
-GO
-
+(6, 3), (6, 5), (6, 7), (6, 9),
 -- For User 7
-INSERT INTO [dbo].[PlayerFav] ([UserID], [PlayerID]) VALUES 
-(7, 2),
-(7, 4),
-(7, 8)
-GO
-
+(7, 2), (7, 4), (7, 8),
 -- For User 8, assigning 5 favorite players as an example
-INSERT INTO [dbo].[PlayerFav] ([UserID], [PlayerID]) VALUES 
-(8, 1),
-(8, 3),
-(8, 5),
-(8, 7),
-(8, 9)
-GO
-
+(8, 1), (8, 3), (8, 5), (8, 7), (8, 9),
 -- For User 9
-INSERT INTO [dbo].[PlayerFav] ([UserID], [PlayerID]) VALUES 
-(9, 10),
-(9, 8),
-(9, 6)
-GO
-
+(9, 10), (9, 8), (9, 6),
 -- For User 10, let's assign 4 favorites
-INSERT INTO [dbo].[PlayerFav] ([UserID], [PlayerID]) VALUES 
-(10, 5),
-(10, 7),
-(10, 9),
-(10, 2)
+(10, 5), (10, 7), (10, 9), (10, 2)
 GO
