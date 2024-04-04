b6a00c7b9b6dd594996adbfe17b6b5beff7815b1
MISTUser <MISTUser@lab8L4JUY>
2024-04-04 15:15:43 -0400

    Added TeamPage.cshtml and TermsandConditions.cshtml pages

---
+++ 353HW1Carr/Pages/Shared/TermsandConditions.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace _353HW1Carr.Pages.Shared
+{
+    public class TermsandConditionsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
