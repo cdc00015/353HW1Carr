3e34faded2a40a1c77ae4b6917645b182843fc73
cdc00015 <cdc00015@mix.wvu.edu>
2024-04-06 17:03:28 -0400

    Added Cross Site Configuration to Program in API Project

--- NFLWeatherAppAPI/Program.cs
+++ NFLWeatherAppAPI/Program.cs
@@ -19,6 +19,18 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowMyRazorPagesApp",
+        builder =>
+        {
+            builder.WithOrigins("https://localhost:7276/")
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+        }
+        
+        )
+})
 
 //builder.Services.AddRazorPages();
 
