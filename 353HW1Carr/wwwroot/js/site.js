dd431b1b3027310d96dffbd5ec9288fb87596310
cdc00015 <cdc00015@mix.wvu.edu>
2024-04-06 16:42:26 -0400

    Updated site js and added html to playerDetails

--- 353HW1Carr/wwwroot/js/site.js
+++ 353HW1Carr/wwwroot/js/site.js
@@ -2,6 +2,8 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+/*
+This is for show the visability of JLove stats
 <script>
     function toggleTableVisibility() {
   var table = document.getElementById('jlStats');
@@ -13,3 +15,13 @@
 }
 
 </script>
+*/
+
+async function displayPlayerDetails(PlayerID) {
+    const response = await fetch('https://localhost:7095/api/Players/Basic/${PlayerID}')
+    const data = await response.json();
+
+    //Changing the HTML and making it visable
+    document.getElementById('playerName').innerHTML = data[0].firstName;
+    document.getElementById('playerName').style.visibility = 'visible';
+}
