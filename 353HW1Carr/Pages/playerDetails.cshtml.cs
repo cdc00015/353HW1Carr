3b81d52349e3ee7a879a8950e82d671a5e3291e6
cdc00015 <cdc00015@mix.wvu.edu>
2024-04-06 15:56:50 -0400

    Created playerDetails Page

---
+++ 353HW1Carr/Pages/playerDetails.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace _353HW1Carr.Pages
+{
+    public class playerDetailsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
